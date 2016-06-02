4251a16b6e6a188e70a6132a2c8cef47e7934e81
MarkuBu <markus.burrer@gmail.com>
2016-06-02 10:44:12 +0200

    Jungle sounds added

--- forests/init.lua
+++ forests/init.lua
@@ -1,3 +1,40 @@
+local jungle_sound_counter = 0
+local jungle_sound_timer = 0
+local jungle_sound_last_time = os.clock()
+minetest.register_abm({
+  nodenames = {"default:jungletree"},
+  neighbors = {"default:jungleleaves"},
+  interval = 10,
+  chance = 5000,
+  action = function(pos, node, active_object_count, active_object_count_wider)
+    if jungle_sound_timer > 10 then
+      minetest.sound_play("ambplus_jungle", {
+        pos = pos,
+        catch_up = false,
+        max_hear_distance = 45,
+        gain = 1,
+      })
+      jungle_sound_timer = 0
+    end
+    jungle_sound_timer = jungle_sound_timer + os.clock() - jungle_sound_last_time
+    jungle_sound_last_time = os.clock()
+  end,
+})
+
+--~ minetest.register_abm({
+  --~ nodenames = {"default:pine_tree"},
+  --~ neighbors = {"default:pine_needles"},
+  --~ interval = 20,
+  --~ chance = 5000,
+  --~ action = function(pos, node, active_object_count, active_object_count_wider)
+    --~ print('jungleabm')
+    --~ minetest.sound_play("ambplus_jungle", {
+      --~ pos = pos,
+      --~ max_hear_distance = 50,
+      --~ gain = 1,
+    --~ })
+  --~ end,
+--~ })
 --~ local timer = 0
 --~ local counter = 0
 --~ minetest.register_globalstep(function(dtime)
